32c5522a77280336f634417d701feabb9c287819
Gerritsensolutions <45983573+Gerritsensolutions@users.noreply.github.com>
2018-12-18 18:42:06 -0800

    Add files via upload

---
+++ Depth-First Search/hw7client.cpp
@@ -0,0 +1,71 @@
+//==========================================
+// HW#: HW7 DFS client using dgraph.h
+// Name: Tyler Gerritsen
+// File type: client hw7client.cpp 
+//==========================================
+
+// Be sure to include dgraph, slist and stack
+#include "dgraph.h"
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+
+// This client uses the dgraph class and stack class
+// to do depth first traversal of the graph stored in table.txt
+int main()
+{
+  dgraph graph;
+  stack s;
+  // use the algorithm (read carefully) in the HW7
+  // assignment sheet
+  
+  // these variables will come in handy in the loop. They enhance readability
+  // and make the code more concise
+  char vertex = 'A';
+  int degree = graph.findOutDegree('A');
+  bool isMarked = graph.isMarked(vertex);
+  slist adjacency = graph.findAdjacency('A');
+  int visitNumber = 1;
+  char adjacentVertex;
+
+  graph.fillTable();
+  graph.displayGraph();
+  s.push(vertex);
+  
+  while ( !s.isEmpty() ) {
+    s.pop(vertex);
+    //degree = graph.findOutDegree(vertex);
+    //isMarked = graph.isMarked(vertex);
+    cout << "Removed " << vertex << " from the stack" << endl;
+    if ( !graph.isMarked(vertex) ) {
+      //cout << "isMarked is no" << endl; 
+      graph.visit(visitNumber, vertex);
+      cout << "Visit " << vertex << " as " << visitNumber << endl;
+      visitNumber++;
+      adjacency = graph.findAdjacency(vertex);
+      if ( adjacency.isEmpty() ) {
+	cout << "Deadend reached - backup" << endl;
+      }
+      else { 
+	cout << "pushing... ";
+	adjacency.displayAll();
+	while ( !adjacency.isEmpty() ) {
+	  adjacency.deleteRear(adjacentVertex);
+	  //cout << "Adjacent vertex: " << adjacentVertex << endl;
+	  s.push(adjacentVertex);
+	}
+	cout << "Current Stack: " << endl;
+	s.displayAll();
+      }
+    }
+    else { 
+      cout << vertex << " has been visited already - backup" << endl;
+      cout << "Current Stack: " << endl;
+      s.displayAll();
+      cout << endl;
+    }
+    graph.displayGraph();
+  }
+
+}
---
+++ Depth-First Search/stack.h
@@ -0,0 +1,86 @@
+//CS311 Yoshii 
+//INSTRUCTION:
+//Stack class header file template based on Notes-1
+//You must complete the ** parts and then complete stack.cpp
+//EMACS HINT:
+//  control-S does searches
+//  Tab on each line will indent perfectly for C++
+
+// =======================================================
+// HW#: HW1P1 stack
+// Your name: Tyler Gerritsen
+// Compiler:  g++ 
+// File type: headher file stack.h
+//=======================================================
+
+//----- Globally setting up the aliases ----------------
+
+const int MAX = 10;   // The MAX number of elements for the stack
+                      // MAX is unknown to the client
+
+typedef char el_t;     // the el_t type is ** for now
+                      // el_t is unknown to the client
+//-------------------------------------------------------
+
+
+class stack
+{ 
+  
+ private: // to be hidden from the client
+  
+   el_t     el[MAX];       // el is  an array of el_t's
+   int      top;           // top is index to the top of stack
+
+ public:  // available to the client
+  
+  // Add exception handling classes here  
+   class Overflow{};  // empty class thrown as overflow exception
+   class Underflow{};  // empty class thrown as underflow exception
+  
+  // prototypes to be used by the client
+  // Note that no parameter variables are given
+
+  stack();   // constructor to create an object
+  ~stack();  // destructor  to destroy an object
+  
+  // PURPOSE: if not full, enters an element at the top;
+  //          otherwise throws an exception - Overflow
+  // PARAMETER: pass the element to be pushed
+  void push(el_t);
+  
+  // PURPOSE: if not empty, removes and gives back the top element;
+  //          otherwise throws an exception - Underflow
+  // PARAMETER: provide variable to receive the popped element (by ref)
+  void pop(el_t&);
+  
+  // PURPOSE: if not empty, gives the top element without removing it;
+  //          otherwise, throws an exception - Underflow
+  // PARAMETER: variable passed by reference which is the top of the stack
+  void topElem(el_t&);
+  
+  // ** Must add good comments for each function - See Notes1B
+
+  // PURPOSE: returns true if the stack is empty, false otherwise
+  //
+  // PARAMETER: none
+  bool isEmpty();
+
+  // PURPOSE: returns true if the stack is full, false otherwise
+  //
+  // PARAMETER: none
+  bool isFull();
+
+  // PURPOSE: prints a visual representation of all elements in the stack
+  // 
+  // PARAMETER: none
+  void displayAll();
+
+  // PURPOSE: clear the stack of all elements
+  // 
+  // PARAMETER: none
+  void clearIt();
+  
+};  
+
+// Note: semicolon is needed at the end of the header file
+
