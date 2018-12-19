8e58ef95260c2e31519573a395d189e64fb80eae
Gerritsensolutions <45983573+Gerritsensolutions@users.noreply.github.com>
2018-12-18 18:41:31 -0800

    Add files via upload

---
+++ Binary Tree/unit-tester.cpp
@@ -0,0 +1,15 @@
+// Unit tester for functions
+
+using namespace std;
+#include <iostream>
+#include "binstree.h"
+
+int main() {
+  BST tree;
+  tree.InsertVertex(1);
+  tree.InsertVertex(2);
+  tree.InsertVertex(3);
+  tree.InsertVertex(4);
+  tree.InsertVertex(5);
+  tree.Display();
+}
